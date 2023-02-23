44bf07d49241df8133922fbf902806f3cd1e7714
Mindy Ng <mindyng85@gmail.com>
2023-02-23 14:01:14 -0800

    Update Upsell_Transactions.sql

--- SQL/Upsell_Transactions.sql
+++ SQL/Upsell_Transactions.sql
@@ -1,3 +1,30 @@
+-- Upsell Transactions
+-- 29
+-- Question
+-- Solution
+-- We’re given a table of product purchases. Each row in the table represents an individual user product purchase.
+
+-- Write a query to get the number of customers that were upsold by purchasing additional products.
+
+-- Note: If the customer purchased two things on the same day that does not count as an upsell as they were purchased within a similar timeframe.
+
+-- Example:
+
+-- Input:
+
+-- transactions table
+
+-- Column  Type
+-- id  INTEGER
+-- user_id INTEGER
+-- created_at  DATETIME
+-- product_id  INTEGER
+-- quantity  INTEGER
+-- Output:
+
+-- Column  Type
+-- num_of_upsold_customers INTEGER
+
 --filter out same day/time tx
 --get orders per user ordered by order date
 --filter on orders after first order
