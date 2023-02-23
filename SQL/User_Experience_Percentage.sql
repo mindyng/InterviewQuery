a3dbc43cec3daa33367e092d06cd115144aa692f
Mindy Ng <mindyng85@gmail.com>
2023-02-23 14:02:06 -0800

    Update User_Experience_Percentage.sql

--- SQL/User_Experience_Percentage.sql
+++ SQL/User_Experience_Percentage.sql
@@ -1,3 +1,31 @@
+-- User Experience Percentage
+-- 6
+-- Question
+-- Solution
+-- Editor
+-- Details
+-- Submissions
+-- Given a table called user_experiences, write a query to determine the percentage of users that held the title of “Data Analyst” immediately before holding the title “Data Scientist”.
+
+-- Immediate is defined as the user holding no other titles between the “Data Analyst” and “Data Scientist” roles.
+
+-- Example:
+
+-- Input:
+
+-- user_experiences table
+
+-- Column  Type
+-- id  INTEGER
+-- position_name VARCHAR
+-- start_date  DATETIME
+-- end_date  DATETIME
+-- user_id INTEGER
+-- Output:
+
+-- Column  Type
+-- percentage  FLOAT
+
 WITH cte AS
   (SELECT * ,
           LAG(position_name, 1) OVER (PARTITION BY user_id
